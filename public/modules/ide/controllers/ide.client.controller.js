f186b2bb1a3554720cd5eb6de2cd2d177ab3c12f
Joshua Jung <wbqdinno@gmail.com>
2014-11-21 06:47:47 +0900

    Add render content function.

--- public/modules/ide/controllers/ide.client.controller.js
+++ public/modules/ide/controllers/ide.client.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('ide').controller('IdeController', ['$scope', '$document',
 	function($scope, $document) {
+		// Inject workspace after page is loaded.
 		$document.ready(function() {
 			Blockly.inject(document.getElementById('blocklyDiv'),
 				{
@@ -9,7 +10,19 @@ angular.module('ide').controller('IdeController', ['$scope', '$document',
 					toolbox: document.getElementById('toolbox')
 				}
 			);
-			//Blockly.addChangeListener(renderContent);
+			Blockly.addChangeListener(renderContent);
 		});
+
+		// Render contents with pretty print.
+		var renderContent = function () {
+			var content = document.getElementById('code');
+			var code = Blockly.cake.workspaceToCode();
+			content.textContent = code;
+			if (typeof prettyPrintOne === 'function') {
+				code = content.innerHTML;
+				code = prettyPrintOne(code, 'c');
+				content.innerHTML = code;
+			}
+		}
 	}
 ]);
