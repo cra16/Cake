290039f55a567ae10bc4fdd5e9cf68a880020ba2
Grace Yang <hjooyang@gmail.com>
2015-02-08 17:18:38 +0900

    core.client.config.js 파일 추가 ( Cake backend auth filter)

---
+++ public/modules/core/config/core.client.config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by graceyang on 15. 2. 8..
+ */
+
+angular.module('core').config(['$httpProvider', function ($httpProvider) {
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    //ZR backend auth filter
+    $httpProvider.interceptors.push(['$q', function($q) {
+        return {
+            request: function(config) {
+                if(gapi && gapi.auth && typeof gapi.auth.getToken === 'function' && gapi.auth.getToken() !== null) {
+                    config.headers['authorization'] = gapi.auth.getToken().access_token;
+                }
+                return config;
+            }
+        };
+    }]);
+}])
