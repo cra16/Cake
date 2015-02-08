f0c331c19998e6903b5cfe952fd21f421f76575b
Grace Yang <hjooyang@gmail.com>
2015-02-08 15:06:58 +0900

    headerController에 newProject, getUser, signOut 함수 추가

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,16 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus',
-	function($scope, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 'Menus', 'drive',
+	function($scope, Authentication, Menus, drive) {
+
+        /**
+         * Requests authorization from the user. Redirects to the previous target
+         * or to create a new doc if no other action once complete.
+         */
+
+        $scope.clientId = config.appId;
+        $scope.scopes = config.scopes;
+
 		$scope.authentication = Authentication;
 		$scope.isCollapsed = false;
 		$scope.menu = Menus.getMenu('topbar');
@@ -14,5 +23,43 @@ angular.module('core').controller('HeaderController', ['$scope', 'Authentication
 		$scope.$on('$stateChangeSuccess', function() {
 			$scope.isCollapsed = false;
 		});
+
+        $scope.getUser = function() {
+            drive.getUser(function(data) {
+                $timeout(function() {
+                    if(data && data.displayName) {
+                        $scope.displayName = data.displayName;
+                    }
+                    else {
+                        $scope.displayName = null;
+                    }
+                    $scope.showLogin = true;
+                });
+            });
+        };
+        $scope.getUser();
+
+        $scope.newProject = function() {
+            drive.newProject();
+        };
+
+        $scope.openProject = function() {
+            drive.openProject(function(id, name) {
+                $timeout(function() {
+                    $location.url('/ide/' + id + '/');
+                });
+            });
+        };
+
+
+        $scope.signOut = function() {
+            gapi.auth.signOut();
+            $location.path('/');
+        }
+        gapi.client.setApiKey(config.simpleApiKey);
+        //Give global reference for login callback
+        angular.module('core').navbarScope = $scope; //필요한 것일까?
 	}
 ]);
+
+
