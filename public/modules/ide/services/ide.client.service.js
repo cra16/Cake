b8c75addf7910754a4f5423976f7a6cdf24f9777
Joshua Jung <wbqdinno@gmail.com>
2015-06-03 19:06:45 +0900

    Seperate compile and project route

--- public/modules/ide/services/ide.client.service.js
+++ public/modules/ide/services/ide.client.service.js
@@ -3,7 +3,7 @@
 //Articles service used for communicating with the articles REST endpoints
 angular.module('ide').factory('Compile', ['$resource',
     function($resource) {
-        return $resource('ide');
+        return $resource('projects/compile');
     }
 ]);
 
